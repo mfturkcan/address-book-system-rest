69dddbe3adc6316b66aad154638f1a7f32a4b56d
mfturkcan <bymehmet45tr@gmail.com>
2022-03-21 20:21:08 +0300

    Add transactional

--- src/main/java/com/mfturkcan/addressbooksystemrest/repositories/BookUserRepository.java
+++ src/main/java/com/mfturkcan/addressbooksystemrest/repositories/BookUserRepository.java
@@ -4,11 +4,13 @@ import com.mfturkcan.addressbooksystemrest.models.BookUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface BookUserRepository extends JpaRepository<BookUser, Integer> {
     Optional<BookUser> findByUsername(String username);
     Optional<List<BookUser>> findByDepartmentIgnoreCaseContaining(String department);
